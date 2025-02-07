d69ef4d9ad05952dc689f70c19d498be964332f6
iain <iainross24.7@gmail.com>
2025-02-07 10:05:06 +0000

    add approved domains list#

--- unsubscriber.py
+++ unsubscriber.py
@@ -6,10 +6,13 @@ import os
 import chardet
 import requests
 
+APPROVED_DOMAINS_FILE = "approved_domains.txt"
+
 def connect_to_mail(username, password):
     mail = IMAPClient("imap.gmail.com")
     mail.login(username, password)
     mail.select_folder("inbox")
+    print("Successfully connected to email server!")
     return mail
 
 def decode_content(content):
@@ -19,7 +22,7 @@ def decode_content(content):
     try:
         return content.decode(detected_encoding, errors="ignore")
     except (UnicodeDecodeError, AttributeError):
-        print("Could not decode email content")
+        print("Could not decode email content.")
         return None
 
 def extract_links_from_html(message):
@@ -34,15 +37,20 @@ def extract_links_from_html(message):
                 # get the text surrounding the <a> tag
                 surrounding_text = link.find_parent().get_text(strip=True).lower()
                 if ("unsubscribe" in link_text or "unsubscribe" in surrounding_text) and ("http" in link["href"] or "https" in link["href"]):
-                    print("Found a link: " + link["href"])
+                    print(f"Found a link: {link["href"]}.")
                     yield link["href"]
 
-def search_for_links(fetch_results):
+def search_for_links(fetch_results, approved_domains):
     # dictionary containing message id keys, and unsubscribe link values
     message_unsubscribe_links = {}
 
     for message_id, fetch_result in fetch_results:
         message = email.message_from_bytes(fetch_result[b'RFC822'])
+
+        # if email from list of approved senders, skip this message
+        sender = message["From"]
+        if sender and any(domain in sender for domain in approved_domains):
+            continue
 
         unsubscribe_links = []
         if message.is_multipart():
@@ -59,12 +67,11 @@ def search_for_links(fetch_results):
 def read_emails(mail):
     mail.idle()
     response = mail.idle_check(timeout=300)
+    mail.idle_done()
 
     if not response:
         mail.noop()
         return
-
-    mail.idle_done()
 
     unread_emails = mail.search('UNSEEN')
     if not unread_emails:
@@ -73,24 +80,34 @@ def read_emails(mail):
     # RFC822 tells the email server to provide all contents of the email
     return mail.fetch(unread_emails, ['RFC822']).items()
 
-
 def click_links(unsubscribe_links):
-    try:
-        for link in unsubscribe_links:
+    # TODO: there is an issue here - if the link redirects to some website where more information is required, the
+    # return code will be 200, but the unsubscribe attempt will not have been successful
+    for link in unsubscribe_links:
+        try:
             response = requests.get(link)
             if response.status_code != 200:
+                print(f"Status code {response.status_code} was received when clicking the link: {link}.")
                 return False
-        return True
-    except Exception:
-        return False
+        except Exception as e:
+            print(f"An exception occurred clicking the link: {link}.\n{e}")
+            return False
+    return True
 
 def move_email(mail, message_id, label):
     try:
         mail.move([message_id], label)
         mail.remove_flags([message_id], ["\\Seen"])
-    except Exception:
-        print("Could not move email to " + label)
+    except Exception as e:
+        print(f"Could not move email to {label}.\n{e}")
 
+def read_approved_domains():
+    file = open(APPROVED_DOMAINS_FILE, "r")
+    domains = [domain for domain in file]
+    if domains:
+        print("Successfully read the list of approved domains!")
+        print("\n".join(domains))
+    return domains
 
 def main():
     load_dotenv()
@@ -98,10 +115,12 @@ def main():
     password = os.getenv("PASSWORD")
     mail = connect_to_mail(username, password)
 
+    approved_domains = read_approved_domains()
+
     while True:
         try:
             fetch_results = read_emails(mail)
-            message_unsubscribe_links = search_for_links(fetch_results)
+            message_unsubscribe_links = search_for_links(fetch_results, approved_domains)
             if message_unsubscribe_links:
                 for message_id, unsubscribe_links in message_unsubscribe_links.items():
                     if click_links(unsubscribe_links):
@@ -109,10 +128,8 @@ def main():
                     else:
                         move_email(mail, message_id, "to-unsubscribe")
         except Exception as e:
-            print(f"An error occurred, re-logging into email and retrying\n{e}")
-            mail.logout()
-            mail.login(username, password)
-
+            print(f"An error occurred, re-logging into email and retrying.\n{e}")
+            mail = connect_to_mail(username, password)
 
 
 if __name__ == '__main__':
